7a55c0a2fd25699aeb6489a7f6894a63f0859f4f
Erick Vieira <magaiverstz@gmail.com>
2023-08-06 20:39:24 -0300

    Initial Strucuture

---
+++ spike.designpatterns.backend/Interface/Controllers/Behavioral/StrategyController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Interface.Controllers.Behavioral;
+
+[ApiController]
+[Route("[controller]")]
+public class StrategyController : ControllerBase
+{
+    public StrategyController()
+    {}
+
+    [HttpGet]
+    public async Task<IActionResult> GetStrategy() => Ok();
+}
